b9803a9f6f6bafc18d591627dba0456242aa13f7
Eli Hart <elihart@stanford.edu>
2013-06-30 20:01:54 -0700

    "rails project created"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stocks::Application.config.secret_token = '77f2c41f5506813d62a3e58d95ac5692879f2dc4f0ecd552abc8d8fb37a44aee33dc63af5bdbc77262c989a1ebfe997ba6cd58385865762c9d2f9557f615adb9'
